d00888548a04fdf05dae5e8052e431b84e2dbfc8
AngelTravieso <angeltraviesoc@gmail.com>
2022-11-23 20:37:37 -0400

    declarando widgets

--- app/src/main/java/com/example/unitconverterapp/MainActivity.java
+++ app/src/main/java/com/example/unitconverterapp/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.unitconverterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView welcome_text, result_text;
+    Button btn;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        welcome_text = findViewById(R.id.welcome_textView);
+        result_text = findViewById(R.id.result_textView);
+        btn = findViewById(R.id.btn);
+        editText = findViewById(R.id.editText);
+
+        // Adding the Functionalities
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
     }
 }
